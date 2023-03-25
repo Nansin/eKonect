da025c73dde1b7daed92918d4c5ea2079e6f78ac
Nansin <nansindn26@gmail.com>
2023-03-25 13:40:49 +0700

    update tag

--- Assets/GameAssets/Scripts/Common/Tags.cs
+++ Assets/GameAssets/Scripts/Common/Tags.cs
@@ -2,9 +2,6 @@ public class Tags
 {
     public const string ENEMY = "Enemy";
     public const string BULLET = "Bullet";
-    public const string TURRET = "Turret";
     public const string PLAYER = "Player";
-    public const string SKILL_FREZZE = "SkillFrezze";
-    public const string BUILDDING = "Buildding";
     public const string BOUNDARY = "Boundary";
 }
